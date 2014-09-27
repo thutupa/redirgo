cdf566cd0c94c505b17cda4d95146fd54895c778
Syam Pannala <syam@google.com>
2014-09-26 17:34:56 -0700

    .

--- client/scripts/actions-controller.js
+++ client/scripts/actions-controller.js
@@ -1,45 +1,45 @@
 var actionsApp = angular.module('ActionsApp', []);
 
 actionsApp.controller('ActionsCtrl', function($scope, $timeout) {
-    $scope.startActionAdd = function() {
-        gapi.client.action.add({
-            redirect: $scope.redirect,
-            words: $scope.words
-        }).execute($scope.endActionAdd);
-	$scope.formDisabled = true;
-	$scope.message = '';
-	$scope.error = '';
-	$scope.searchPhrase = '';
-    }
+  $scope.startActionAdd = function() {
+    gapi.client.action.add({
+        redirect: $scope.redirect,
+        words: $scope.words
+    }).execute($scope.endActionAdd);
+    $scope.formDisabled = true;
+    $scope.message = '';
+    $scope.error = '';
+    $scope.searchPhrase = '';
+  }
 
-    $scope.endActionAdd = function(resp) {
-	$scope.formDisabled = false;
-	if (resp.error) {
-	  $scope.error = resp.error.message;
-	} else {
-	  $scope.redirect = '';
-	  $scope.words = '';
-	  $scope.setMessage('Added');
-	  $scope.fetchItems();
-	}
-	$scope.$apply();
+  $scope.endActionAdd = function(resp) {
+    $scope.formDisabled = false;
+    if (resp.error) {
+      $scope.error = resp.error.message;
+    } else {
+      $scope.redirect = '';
+      $scope.words = '';
+      $scope.setMessage('Added');
+      $scope.fetchItems();
     }
+    $scope.$apply();
+  }
 
-    $scope.clearMessage = function() {
-      $scope.message = '';
-    }
+  $scope.clearMessage = function() {
+    $scope.message = '';
+  }
 
-    $scope.setMessage = function(message) {
-      $scope.message = message;
-      $timeout($scope.clearMessage, 1000);
-    }
+  $scope.setMessage = function(message) {
+    $scope.message = message;
+    $timeout($scope.clearMessage, 1000);
+  }
 
-    $scope.fetchItems = function() {
-      gapi.client.action.list({phrase: $scope.searchPhrase}).execute(function(resp) {
-	$scope.items = resp.items;
-	$scope.$apply();
-      });
-    }
+  $scope.fetchItems = function() {
+    gapi.client.action.list({phrase: $scope.searchPhrase}).execute(function(resp) {
+      $scope.items = resp.items;
+      $scope.$apply();
+    });
+  }
   $scope.items = [];
   $scope.fetchItems();
 });
